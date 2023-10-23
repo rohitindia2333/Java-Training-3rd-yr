c259b946a5b42b693044ae05fa82e36ee4ba0061
Rohit Kushwaha <113816084+rohitindia2333@users.noreply.github.com>
2023-10-23 22:22:02 +0530

    Add files via upload

--- FUNCTION/Salery_fixture.java
+++ FUNCTION/Salery_fixture.java
@@ -1,27 +1,38 @@
 import java.util.Scanner;
 
 public class Salery_fixture {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        public static void main(String[] args) {
-            Scanner sc=new Scanner(System.in);
-            System.out.println("enter the salery amount");
-            double income = sc.nextDouble();
-    
-            double tax = calculateIncomeTax(income);
-            System.out.println("Income: " + income);
-            System.out.println("Income Tax: " + tax);
-        }
-    
-        public static double calculateIncomeTax(double income) {
-            if (income < 10000) {
-                return 0; 
-            } else if (income >= 10000 && income < 20000) {
-                return (income - 10000) * 0.10; 
-            } else if (income >= 20000 && income < 30000) {
-                return 1000 + (income - 20000) * 0.15; 
-            } else {
-                return 2500 + (income - 30000) * 0.25; 
-            }
-        }
+        System.out.print("Enter basic salary: ");
+        double basicSalary = sc.nextDouble();
+
+        double hra = calculateHRA(basicSalary);
+        double da = calculateDA(basicSalary);
+        double ta = 2000;
+        double grossSalary = basicSalary + hra + da + ta;
+
+        double tax = calculateTax(grossSalary);
+
+        double netSalary = grossSalary - tax;
+
+        System.out.println("HRA: " + hra);
+        System.out.println("DA: " + da);
+        System.out.println("TA: " + ta);
+        System.out.println("Gross Salary: " + grossSalary);
+        System.out.println("Tax: " + tax);
+        System.out.println("Net Salary: " + netSalary);
     }
-    
+
+    public static double calculateHRA(double basicSalary) {
+        return 0.30 * basicSalary;
+    }
+
+    public static double calculateDA(double basicSalary) {
+        return 0.20 * basicSalary;
+    }
+
+    public static double calculateTax(double grossSalary) {  //we consider 10% tax for each salery
+        return 0.10 * grossSalary;
+    }
+}
